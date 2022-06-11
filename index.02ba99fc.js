038f06656c820e3d9d456e6982831dadb71e2462
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2022-06-11 05:35:57 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/ice-cream_project@3d89705f979c8479ef46f0aa5f0d59d6fcd4eef6 🚀

---
+++ index.02ba99fc.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0},grabCursor:!0,loop:!0,autoplay:{deley:3e3,disableOnInteraction:!1},speed:900});
+//# sourceMappingURL=index.02ba99fc.js.map

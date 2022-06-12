4a364183bb4a1b2c7f9a9b9be2557df2cddf18de
evgsildev <92385586+evgsildev@users.noreply.github.com>
2022-06-12 09:47:11 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/ice-cream_project@e83694e4ed37ad9d23e45ce96e0cfbba56b17e66 🚀

---
+++ index.80898597.js
@@ -0,0 +1,2 @@
+!function(){var o={openAboutModalBtn:document.querySelector("[about-modal-open]"),closeAboutModalBtn:document.querySelector("[about-modal-close]"),AboutModal:document.querySelector("[about-modal]")};function t(){o.AboutModal.classList.toggle("is-hidden")}o.openAboutModalBtn.addEventListener("click",t),o.closeAboutModalBtn.addEventListener("click",t)}();
+//# sourceMappingURL=index.80898597.js.map

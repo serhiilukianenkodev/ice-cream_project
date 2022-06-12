4b51fd9b5ceec22a25c0e98c065c28dd34965079
dmitry-prokudinov <90647520+dmitry-prokudinov@users.noreply.github.com>
2022-06-12 16:04:01 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/ice-cream_project@5e50fd088f3c25c225bda1e0968b98a4778633bb 🚀

---
+++ index.681d9ab0.js
@@ -0,0 +1,2 @@
+!function(){var e={openHeroModalBtn:document.querySelector("[hero-modal-open]"),closeHeroModalBtn:document.querySelector("[hero-modal-close]"),HeroModal:document.querySelector("[hero-modal]")};function o(){e.HeroModal.classList.toggle("is-hidden")}e.openHeroModalBtn.addEventListener("click",o),e.closeHeroModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.681d9ab0.js.map
